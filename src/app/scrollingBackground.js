a90acd846a7a2054aa1307d183a36c5164f34a26
PavelKhomyakov <pakhom@fab.pasona.tech>
2024-04-02 16:32:01 +0900

    Scrolling fixed

--- src/app/scrollingBackground.js
+++ src/app/scrollingBackground.js
@@ -1,10 +1,9 @@
-// scrollingBackground.js
 export function setupScrollingBackground() {
   const images = ['image1.jpg', 'image2.jpg', 'image3.png'];
   let currentImageIndex = 0;
   const fadeThreshold = 0.8; // Adjust this value to change when the image starts fading out
 
-  return (scrollPosition, maxScroll) => {
+  return (scrollPosition, maxScroll, isScrollingDown) => {
     const background = document.querySelector('.background-container');
     const viewportHeight = window.innerHeight;
 
@@ -12,33 +11,23 @@ export function setupScrollingBackground() {
     const containerHeight = Math.min(viewportHeight, maxScroll - scrollPosition);
     background.style.height = `${containerHeight}px`;
 
-    // Calculate the current image index based on scroll position
-    let newImageIndex = 0;
-    if (maxScroll > 0) {
-      newImageIndex = Math.min(Math.floor((scrollPosition / maxScroll) * images.length), images.length - 1);
+    // Calculate the current image index based on scroll position and direction
+    if (isScrollingDown) {
+      currentImageIndex = Math.min(Math.floor((scrollPosition / maxScroll) * images.length), images.length - 1);
+    } else {
+      currentImageIndex = Math.max(Math.floor((scrollPosition / maxScroll) * images.length), 0);
     }
 
-    if (newImageIndex !== currentImageIndex) {
-      currentImageIndex = newImageIndex;
-      const nextImageUrl = `images/${images[currentImageIndex]}`;
-      const nextImage = new Image();
-      nextImage.src = nextImageUrl;
-      nextImage.onload = () => {
-        background.style.backgroundImage = `url(${nextImageUrl})`;
-        setTimeout(() => {
-          background.style.opacity = 1; // Fade in the new image
-        }, 50); // Delay to allow the background image to change
-      };
-      background.style.opacity = 0; // Start with opacity 0
-    }
-
-    // Reset background image when scroll position is at the top
-    if (scrollPosition === 0) {
-      currentImageIndex = 0;
-      const imageUrl = `images/${images[currentImageIndex]}`;
-      background.style.backgroundImage = `url(${imageUrl})`;
-      background.style.opacity = 1; // Reset opacity when changing images
-    }
+    const nextImageUrl = `images/${images[currentImageIndex]}`;
+    const nextImage = new Image();
+    nextImage.src = nextImageUrl;
+    nextImage.onload = () => {
+      background.style.backgroundImage = `url(${nextImageUrl})`;
+      setTimeout(() => {
+        background.style.opacity = 1; // Fade in the new image
+      }, 50); // Delay to allow the background image to change
+    };
+    background.style.opacity = 0; // Start with opacity 0
 
     // Fade out the background image when reaching the fade threshold
     if (scrollPosition > fadeThreshold * maxScroll) {
