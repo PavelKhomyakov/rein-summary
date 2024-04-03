83c94f026e45a36b17db4a5d7aaa749a0266a37c
PavelKhomyakov <pakhom@fab.pasona.tech>
2024-04-03 17:23:10 +0900

    Removed unnecessary lines and added tooltip sample on main page

--- src/app/scrollingBackground.js
+++ src/app/scrollingBackground.js
@@ -2,7 +2,6 @@ export function setupScrollingBackground() {
   const images = ['image1.jpg', 'image2.jpg', 'image3.png', 'image4.jpeg', 'image5.png', 'image6.png', 'image7.png'].map(image => `images/${image}`);
   const numImages = images.length;
   let currentImageIndex = 0;
-  const fadeThreshold = 0.8; // Adjust this value to change when the image starts fading out
 
   const imageElements = images.map(imageUrl => {
     const image = new Image();
@@ -12,8 +11,6 @@ export function setupScrollingBackground() {
 
   return (scrollPosition, maxScroll) => {
     const background = document.querySelector('.background-container');
-    const opacity = Math.max(0, 1 - ((scrollPosition - (fadeThreshold * maxScroll)) / (fadeThreshold * maxScroll)));
-    background.style.opacity = opacity.toString();
 
     // Calculate the current image index based on scroll position
     const percentage = Math.min(scrollPosition / maxScroll, 1);
